8cd1de682f69d5c89addd3a3f841f6bcf37b27ed
Zwuiix <71025924+Zwuiix-cmd@users.noreply.github.com>
2026-04-17 15:11:29 +0200

    Update to 1.26.10

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@
 #include "Util/GameUtil.h"
 #include "Util/Terminal.h"
 
-const std::string SIGNATURE = "74 0B F3 44 0F 5D 3D ? ? ? ?";
+const std::string SIGNATURE = "74 ? F3 0F 5D 35";
 
 int main()
 {
     Terminal::setup();
-    if(!GameUtil::close()) {
+    if (!GameUtil::close()) {
         LoggerUtil::log("(!) Starting Minecraft.Windows.exe", std::chrono::milliseconds(25));
         system("start minecraft://");
     }
@@ -19,7 +19,7 @@ int main()
     {
         GameUtil::handle = GameUtil::GetProcessByName("Minecraft.Windows.exe");
         GameUtil::module = GameUtil::GetModule(GameUtil::handle);
-        if(GameUtil::handle != nullptr && GameUtil::module != nullptr) LoggerUtil::success("(+) Game found!", std::chrono::milliseconds(10));
+        if (GameUtil::handle != nullptr && GameUtil::module != nullptr) LoggerUtil::success("(+) Game found!", std::chrono::milliseconds(10));
     }
     Terminal::clear();
 
@@ -27,7 +27,7 @@ int main()
     LoggerUtil::warning("(?) Scan in progress...", std::chrono::milliseconds(20));
 
     void* sig = GameUtil::scanSig(SIGNATURE);
-    if(sig == nullptr) {
+    if (sig == nullptr) {
         LoggerUtil::error("(-) Not found Reach sig.", std::chrono::milliseconds(10));
         std::this_thread::sleep_for(std::chrono::seconds(5));
         return 0;
@@ -46,17 +46,16 @@ int main()
     uintptr_t addrNOP = reinterpret_cast<uintptr_t>(sig);
     uintptr_t addrBytesCREATIVE = addrNOP + 0x2;
 
-    uint8_t instructionBytesCREATIVE[9];
-    GameUtil::readMemory(reinterpret_cast<void*>(addrBytesCREATIVE), instructionBytesCREATIVE, sizeof(instructionBytesCREATIVE));
+    constexpr size_t LEN = 8;
+    uint8_t ins[LEN]{};
+
+    GameUtil::readMemory(reinterpret_cast<void*>(addrBytesCREATIVE), ins, sizeof(ins));
+
+    auto disp = static_cast<int32_t>((ins[4]) | (ins[5] << 8) | (ins[6] << 16) | (ins[7] << 24));
+    auto addrReach = static_cast<uintptr_t>(static_cast<int64_t>(addrBytesCREATIVE) + LEN + disp);
 
     GameUtil::nopBytes(reinterpret_cast<void*>(addrNOP), 2);
-    GameUtil::patchBytes(reinterpret_cast<void*>(addrBytesCREATIVE + 9 +
-        (
-            instructionBytesCREATIVE[5] |
-            (instructionBytesCREATIVE[6] << 8) |
-            (instructionBytesCREATIVE[7] << 16) |
-            (instructionBytesCREATIVE[8] << 24)
-            )),&reach, sizeof(float));
+    GameUtil::patchBytes(reinterpret_cast<void*>(addrReach), &reach, sizeof(float));
 
     LoggerUtil::success("(!) Reach successfully injected!", std::chrono::milliseconds(25));
     std::this_thread::sleep_for(std::chrono::seconds(5));
